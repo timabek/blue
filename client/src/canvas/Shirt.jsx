925fc63ee36aeb16526166e138d64b41e89f0697
timaDev00 <timabeknarynbekovich@gmail.com>
2023-05-15 18:07:45 +0600

    add it

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -8,7 +8,7 @@ import state from '../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/tols.glb');
+  const { nodes, materials } = useGLTF('/tolsit.glb');
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from 'valtio';
 
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: '#3e4242',
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: './threejs.png',
